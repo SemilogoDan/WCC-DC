e0322286d05488c51907c35bd27b472cca5828fa
semilogoolusola <semilogoolusola@ymail.com>
2025-02-26 20:54:19 +0100

    to be corrected

--- add_CFO.m
+++
@@ -1,8 +0,0 @@
-function distortedarray = add_CFO(inputarray, CFO, Ts)
-%ADD_CFO Summary of this function goes here
-%   Detailed explanation goes here
-    distortedarray = zeros(size(inputarray));
-    t = Ts*[0:length(inputarray)-1];
-    distortedarray = inputarray.*exp(1j*2*pi*CFO*t);
-end
-
